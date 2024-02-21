cf3a198a629484a25537cc245ae20625f6d7f30e
ZenithClown <debmalyapramanik.005@gmail.com>
2024-02-22 01:45:36 +0530

    🛠🎉 check report of all the market at once - check missing records/to update records for all the tracked iex product market types

--- schema/dev/stored-procedures/market_snapshot.sql
+++ schema/dev/stored-procedures/market_snapshot.sql
@@ -4,21 +4,22 @@ A list of stored procedures for the market snapshot table under the
 mentioned under the `sp create` command.
 
 ```sql
-CALL `dev.market_snapshot_report`(<{IN product_type VARCHAR(16)}>);
+CALL `dev.market_snapshot_report`();
 ```
 
 Author  : Debmalya Pramanik, nxLogics
 Contact : neuralNOD@gmail.com
-Version : v0.0.1
+Version : v0.0.2
 
 Copywright © [2024] Debmalya Pramanik, nxLogics
 ********************************************************************/
 
 DELIMITER &&
-CREATE PROCEDURE `dev.market_snapshot_report`(IN product_type VARCHAR(16))
+CREATE PROCEDURE `dev.market_snapshot_report`()
 BEGIN
     SELECT
-        MIN(TransactionDate) AS data_available_form
+        IEXProdMarketType
+        , MIN(TransactionDate) AS data_available_form
         , MAX(TransactionDate) AS data_available_till
         , DATEDIFF(CURRENT_DATE(), MAX(TransactionDate)) AS missing_days_till_today
         , FORMAT(DATEDIFF(MAX(TransactionDate), MIN(TransactionDate)) + 1, 0) AS num_days_records_available
@@ -29,6 +30,9 @@ BEGIN
             END AS remarks
         , FORMAT((DATEDIFF(MAX(TransactionDate), MIN(TransactionDate)) + 1) * 96 - COUNT(*), 0) AS num_blocks_missing
     FROM `apps.lumos.tblMarketSnapshot`
-    WHERE IEXProdMarketType = product_type;
+    GROUP BY IEXProdMarketType
+    ORDER BY
+		num_blocks_missing DESC -- missing block information is reported at the top
+        , missing_days_till_today DESC; -- followed by the missing data update table till today
 END &&
 DELIMITER ;
